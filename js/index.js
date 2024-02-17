3dec35224f4cd6d1584b22104ed798846d1d9e6f
RAJANIKANTA BISWAL <rajanikantabiswal15@gmail.com>
2024-02-17 12:16:06 +0530

    addd

--- js/index.js
+++ js/index.js
@@ -1,51 +1,96 @@
-let navbar= document.querySelector('.header .navbar');
-let searchForm =  document.querySelector('.header .search-form');
-let loginForm = document.querySelector('.header .login-form');
-let contactInfo = document.querySelector('.contact-info');
+let navbar = document.querySelector(".header .navbar");
+let searchForm = document.querySelector(".header .search-form");
+let loginForm = document.querySelector(".header .login-form");
+let contactInfo = document.querySelector(".contact-info");
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    loginForm.classList.remove('active');
-    contactInfo.classList.remove('active');
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  contactInfo.classList.remove("active");
 };
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
-    contactInfo.classList.remove('active');
-}
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+  contactInfo.classList.remove("active");
+};
 
-document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    contactInfo.classList.remove('active');
-}
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  contactInfo.classList.remove("active");
+};
 
-document.querySelector('#info-btn').onclick = () =>{
-    contactInfo.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    loginForm.classList.remove('active');
-}
-document.querySelector('#close-contact-info').onclick = () =>{
-    contactInfo.classList.remove('active');
-}
-window.onscroll =()=>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    loginForm.classList.remove('active');
-    contactInfo.classList.remove('active');
-}
+document.querySelector("#info-btn").onclick = () => {
+  contactInfo.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+document.querySelector("#close-contact-info").onclick = () => {
+  contactInfo.classList.remove("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  contactInfo.classList.remove("active");
+};
 
 var swiper = new Swiper(".home-slider", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+lightGallery(document.querySelector(".projects .box-container"));
+
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  loop: true,
+  grabCursor: true,
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 20,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".blogs-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
     loop: true,
     grabCursor: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
     },
   });
-
-  lightGallery(document.querySelector('.projects .box-container'));
